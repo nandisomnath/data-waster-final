de3f0bbf33cf022d9dcca55638f9f1c12de4a9f0
Somnath Nandi <123479436+CodeOfSomnath@users.noreply.github.com>
2024-03-08 18:38:28 +0530

    changing version

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func try(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetUrls() []string {
+	dataOfFile, err := os.ReadFile("urls.txt")
+	try(err)
+	rawStringData := string(dataOfFile)
+	var urls = strings.Split(rawStringData, "\n")
+	return urls
+}
+
+func SetUrls(url string) bool {
+	dataOfFile, err := os.ReadFile("urls.txt")
+	try(err)
+	rawStringData := string(dataOfFile)
+	isContains := strings.Contains(rawStringData, url)
+	file, err := os.OpenFile("urls.txt", os.O_APPEND ,0644)
+	defer file.Close()
+
+	if !isContains {
+		// os.WriteFile("urls.txt", []byte(), os.ModeAppend)
+		_, err = file.WriteString(fmt.Sprintf("%s\n", url))
+		try(err)
+	}
+
+	return isContains
+}
+
+func SendRequest(url string)  {
+	response, err := http.Get(url)
+	try(err)
+	reader := bufio.NewReader(response.Body)
+	var buffer = make([]byte, 2048)
+	_, err = reader.Read(buffer)
+
+	for err != io.EOF {
+		_, err = reader.Read(buffer)
+	}
+}
+
+
+func main() {
+	url := flag.String("u", "", "Enter a url for add")
+	flag.Parse()
+	if *url != "" {
+		isDup := SetUrls(*url)
+		if isDup {
+			fmt.Println("Duplicate: ", *url)
+		} else {
+			fmt.Println("Successfully Added: ", *url)
+		}
+		os.Exit(0)
+	}
+
+	urls := GetUrls()
+
+	for _, url := range urls {
+		SendRequest(strings.TrimSpace(url))
+	}
+
+}
