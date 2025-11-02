4fc16f8adf63700d4c16481038b799bfb0e3fe66
Somnath Nandi <somnathnandi368@gmail.com>
2025-11-02 13:04:29 +0530

    fixed version and less memory use

--- main.py
+++ main.py
@@ -2,24 +2,15 @@ import requests
 import time
 import argparse
 
-
 B = 1
 KB = B * 1000
 MB = KB * 1000
 GB = MB * 1000
 
 
-def get_limit_size(max_speed, percentage_speed_limit):
-    # returns a size/sec in bytes
-    if percentage_speed_limit > 100:
-        print("Unable to use speed limit greater then 100% ")
-        return None
-    
-    download_chunk = (
-        (percentage_speed_limit / 100) # 0.x
-        * (max_speed * MB)
-    )
-
+def get_chunk_size(max_speed):
+    # returns chunk size in bytes from maxspeed in MB/s
+    download_chunk = (max_speed * MB)
     return int(download_chunk)
 
 
@@ -46,7 +37,7 @@ def update_record(total_size_in_bytes):
     
 
 
-def download(url_path, max_speed, percentage_limit):
+def download(url_path, max_speed):
     """
     @param max_speed  MB/s unit
     """
@@ -58,23 +49,31 @@ def download(url_path, max_speed, percentage_limit):
     
   
     total_size_in_bytes = int(res.headers.get("Content-Length", 0))
-    chunk_size = get_limit_size(max_speed, percentage_limit)
-    downloaded_bytes = 0
+    chunk_size = MB
+    downloaded_in_second = 0
+    downloaded_bytes =0
     start = time.time()
 
     for stream in res.iter_content(chunk_size=chunk_size):
-
         end = time.time()
 
         # count the percentage
+        fstart = time.time()
         current_chunk_size = len(stream)
-        downloaded_bytes += current_chunk_size
-        percentage = (downloaded_bytes / total_size_in_bytes) * 100
+        downloaded_in_second += current_chunk_size
         duration = end - start
-        speed = (current_chunk_size / MB) / duration
-        print(f"Download: {percentage:.2f}% ({speed:.2f}MB/s) \r", end="")
-        start = time.time()
-            
+
+        if duration >= 1.0:
+            downloaded_bytes += downloaded_in_second
+            percentage = (downloaded_bytes / total_size_in_bytes) * 100
+            speed = (downloaded_in_second / MB) / duration
+            print(f"Download: {percentage:.2f}% ({speed:.2f}MB/s) \r", end="")
+            downloaded_in_second = 0
+            start = time.time()
+        else:
+            fd = time.time() - fstart
+            start -= fd  # adjusting the start time to exclude processing time
+
     res.close()
     
     # Download is complete 
@@ -89,15 +88,9 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
                                      )
 
-    parser.add_argument("-m", "--max",type=float, default=1.0,  help="max speed of your internet connection in MB/s unit")
-    parser.add_argument("-l", "--limit",type=int, default=100, help="Limit of the downloading speed in percentage [1..100]")
-
+    parser.add_argument("-m", "--max",type=float, default=1.0,  help="max speed to waste data in MB/s unit")
     args = parser.parse_args()
-
     max_speed = args.max
-    percentage_limit = args.limit
-    
-
 
     file = open("urls.txt", "r")
     urls = []
@@ -112,7 +105,7 @@ if __name__ == "__main__":
                 if url == '':
                     continue
                 try:
-                    download(url, max_speed, percentage_limit)
+                    download(url, max_speed)
                 except requests.exceptions.ConnectionError as e:
                     # when there is not internet or any connection
                     pass
